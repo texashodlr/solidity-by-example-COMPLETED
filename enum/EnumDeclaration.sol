8efd9f9d2372f9c05aa5be2f0795665345ba30aa
Conor Devlin <50897652+texashodlr@users.noreply.github.com>
2022-06-17 12:54:59 -0600

    Create EnumDeclaration.sol

---
+++ enum/EnumDeclaration.sol
@@ -0,0 +1,12 @@
+//file that the enum is declared in
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+enum Status {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}
